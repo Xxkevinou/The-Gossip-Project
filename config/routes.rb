f0c11a9b148c5c2b69fe78d116eb041cff78f3a7
Kevinou <sophie-bretzel@laposte.net>
2018-10-30 13:23:51 +0100

    tout va bien

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'gossip/show/:id', to: 'gossip#show'
   get 'index/welcome/:url', to: 'index#welcome'
   get '/', to: 'index#home'
   get 'static_page/contact'
